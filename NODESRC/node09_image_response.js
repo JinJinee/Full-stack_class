f3ae281cee75c6ed87103fb7d46135d7d875eb86
Sujin Shin <wls3147@gmail.com>
2022-03-30 22:43:30 +0900

    commit

---
+++ NODESRC/node09_image_response.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res){
+    fs.readFile(__dirname+'/img/sleepyR.jpeg', function(e, imgSrc){
+        if(!e){
+            res.writeHead(200, {'Content-Type':'image/jpeg'});
+            res.write(imgSrc);
+            res.end();
+        }
+    });
+
+});
+
+server.listen(10006, function(){
+    console.log('server start.....    http://127.0.0.1:10006');
+});
---
+++ NODESRC/node11_html_img_response.js
@@ -0,0 +1,39 @@
+var http = require("http");
+var fs = require("fs");
+
+// 모듈추가하는 방법 : mime
+// npm 명령어를 이용하여 추가
+// >npm install mime@2
+var mime = require('Mime');
+
+var server = http.createServer(function(req, res){
+    var addr = req.url;
+    console.log(addr);
+
+    if(addr == "/home"){
+        // home.html 파일의 내용을 읽어 response에 쓰기를 한다.
+        fs.readFile(__dirname+'/home.html', 'utf-8', function(e, d){
+            if(e){ // 파일읽기 에러
+                console.log('파일읽기 실패... home.html');
+            } else{ // 정상 읽기되었을때
+                res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                res.end(d);
+            }
+        });
+    }else if(addr.indexOf('/img' == 0)){ // 이미지일 경우
+        // 마임구하기
+        var mimeType = mime.getType(addr.substring(1));
+        console.log(mimeType);
+
+        fs.readFile(__dirname+addr, function(error, imgData){
+            if(!error){
+                res.writeHead(200, {'Content-Type':mimeType});
+                res.end(imgData);
+            }
+        });
+    }
+});
+
+server.listen(10007, function(){
+    console.log("server start....... http://127.0.0.1:10007/home");
+})
